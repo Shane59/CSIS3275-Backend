31ac1953954b1f153c36493cd31bb562036cfa36
Dean Marques <deanmarquesjr@gmail.com>
2023-11-27 12:20:29 -0800

    TravelBuddy tour, get, post, delete

---
+++ src/main/java/com/api/csis3275_backend/repositories/TravelBuddyRepository.java
@@ -0,0 +1,23 @@
+package com.api.csis3275_backend.repositories;
+
+import com.api.csis3275_backend.entities.TravelBuddy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public interface TravelBuddyRepository extends JpaRepository<TravelBuddy, Long> {
+    @Query(value = "SELECT * FROM TravelBuddy WHERE applicationStatus = :applicationStatus", nativeQuery = true)
+    List<TravelBuddy> findByApplicationStatus(String applicationStatus);
+
+    @Query(value = "SELECT * FROM TravelBuddy  WHERE travelBuddyID = :travelBuddyID", nativeQuery = true)
+    Optional<TravelBuddy> findByTravelBuddyID(int travelBuddyID);
+
+    @Query(value = "DELETE FROM TravelBuddy  WHERE travelBuddyID = :travelBuddyID AND applicationStatus = :applicationStatus", nativeQuery = true)
+    Optional<TravelBuddy> deleteTravelBuddyByApplicationStatusAndTravelBuddyID(String applicationStatus, int travelBuddyID);
+
+    @Override
+    TravelBuddy save(TravelBuddy travelBuddy);
+}
